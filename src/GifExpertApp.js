b4c8df0b7e57358d50dc94e86dc7a8f548da0422
Rodrigo Poblete <rodrigopoblete.dev@gmail.com>
2021-08-04 16:30:40 -0400

    Initial commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const GifExpertApp = () => {
+
+    const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
+
+    return (
+        <div>
+            <h2>GifExpertApp</h2>
+            <hr />
+        </div>
+    )
+}
